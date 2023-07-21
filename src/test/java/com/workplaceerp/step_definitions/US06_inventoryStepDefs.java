4c0889fe2f2c3fca8e9ea5876e9d10d0426bdf4b
Cydeo-Nuray <yilmaz.nn@gmail.com>
2023-07-21 03:30:24 -0400

    Sprint2; US6

---
+++ src/test/java/com/workplaceerp/step_definitions/US06_inventoryStepDefs.java
@@ -0,0 +1,94 @@
+package com.workplaceerp.step_definitions;
+
+import com.workplaceerp.utilities.BrowserUtils;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import com.workplaceerp.pages.AddToInventory_NY;
+import com.workplaceerp.pages.BasePage;
+import com.workplaceerp.pages.LoginPage;
+
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
+
+public class US06_inventoryStepDefs extends BasePage  {
+    LoginPage loginPage = new LoginPage();
+    AddToInventory_NY addToInventoryNy = new AddToInventory_NY();
+
+
+    @Given("the user is logged in as the POS Manager")
+    public void theUserIsLoggedInAsThePOSManager() {
+        loginPage.login("posmanager155@info.com", "posmanager");
+
+    }
+
+    @When("User clicks on the Inventory tab")
+    public void userClicksOnTheInventoryTab() {
+        BrowserUtils.sleep(5);
+        addToInventoryNy.inventoryTab.click();
+
+    }
+
+    @And("User clicks on the Products tab under the Master Data")
+    public void userClicksOnTheProductsTabUnderTheMasterData() {
+
+        addToInventoryNy.productTab.click();
+    }
+
+    @And("User clicks the CREATE button")
+    public void userClicksTheCREATEButton() {
+        addToInventoryNy.createButton.click();
+    }
+
+    @Given("User enters the product name {string} in the Product Name input box")
+    public void userEntersTheProductNameInTheProductNameInputBox(String product_name) {
+        addToInventoryNy.productNameInput.sendKeys(product_name);
+        BrowserUtils.sleep(10);
+    }
+
+    @And("User clicks the SAVE button")
+    public void userClicksTheSAVEButton() {
+
+        addToInventoryNy.saveButton.click();
+
+    }
+
+    @Then("User-entered {string} should be displayed for the product")
+    public void userEnteredShouldBeDisplayedForTheProduct(String product_name) {
+        Assert.assertEquals(addToInventoryNy.Product.getText(),product_name);
+    }
+
+
+
+    @When("User clicks on a product")
+    public void userClicksOnTheProduct() {
+        addToInventoryNy.newProduct.click();
+    }
+    @And("User clicks on the edit button")
+    public void userClicksOnTheEditButton() {
+
+        addToInventoryNy.clickEditButton.click();
+    }
+
+    @And("User edits the ProductName input box with another {string}")
+    public void userEditsTheProductNameInputBoxWithAnother(String editedName) {
+
+       addToInventoryNy.editedProductInput.clear();
+       addToInventoryNy.editedProductInput.sendKeys(editedName);
+    }
+
+    @And("User clicks the save button")
+    public void userClicksTheSaveButton() {
+        addToInventoryNy.saveButton.click();
+    }
+
+
+    @Then("User should see the {string} information displayed")
+    public void userShouldSeeTheInformationDisplayed(String editedName) {
+        BrowserUtils.sleep(10);
+        Assert.assertEquals(addToInventoryNy.Product.getText(), editedName);
+    }
+}
+
---
+++ src/test/resources/features/addToInventory.feature
@@ -0,0 +1,41 @@
+@B29G9-240
+Feature: Default
+
+	Background:
+		#@B29G9-230
+
+		    Given the user is logged in as the POS Manager
+			And User clicks on the Inventory tab
+			And User clicks on the Products tab under the Master Data
+
+	#*{color:#403294}User Story:{color}* {color:#ff8b00}As a PosManager, I should be able to add new products to inventory.{color}
+	#
+	#*{color:#403294}AC1:{color}* {color:#ff8b00}The user can create a new product by filling only the Product Name input box and after clicking the save button, the information of the customer which the user entered should be displayed.{color}
+	@B29G9-204
+	Scenario Outline: US06-AC01-TC01: Verify that POS Manager can create a new product by only filling the Product Name input box
+		When User clicks the CREATE button
+		And User enters the product name "<product_name>" in the Product Name input box
+		    And    User clicks the SAVE button
+		    Then   User-entered "<product_name>" should be displayed for the product
+		    Examples:
+		      | product_name |
+		      | Book         |
+		      | Ipad         |
+		      | Iphone8      |	
+
+	#*{color:#403294}User Story:{color}* {color:#ff8b00}As a PosManager, I should be able to add new products to inventory.{color}
+	#
+	#*{color:#403294}AC2:{color}* {color:#ff8b00}When clicking any product and then clicking the edit button all the boxes entered information can be editable again.{color}
+	@B29G9-207
+	Scenario Outline: US06-AC02-TC02: Verify that entered information is editable when clicking on the “edit” button on any product.
+		When   User clicks on a product
+		  And  User clicks on the edit button
+		  And   User edits the ProductName input box with another "<editedName>"
+		  And   User clicks the save button
+		  Then  User should see the "<expectedName>" information displayed
+		
+		    Examples:
+		      |editedName   |expectedName|
+		      |Notebooks    |Notebooks   |
+		      |Tablet       |Tablet      |
+		      |Android      |Android     |
